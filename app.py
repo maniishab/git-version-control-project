bacb5d2e37dcf253ef0af2290c8305986ca865b0
manisha <manisha@ip-172-31-28-194.us-east-2.compute.internal>
2026-03-13 04:45:46 +0000

    fix formatting issue

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 print("Hello from DevOps Git Project")
 
-def add(a,b) :
+
+def add(a, b):
     return a + b
 
-def multiply(a,b):
-    return a*b
 
-print(add(2,3))
-print(multiply(3,4))
+def multiply(a, b):
+    return a * b
+
+
+print(add(2, 3))
+print(multiply(3, 4))
